225efab5878094c3a094dfe621c93241391e716a
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2023-10-06 15:33:47 +0900

    버튼화면 파일

---
+++ Practice/hyeran/toy/button/button/buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  buttonApp.swift
+//  button
+//
+//  Created by 김혜란 on 2023/10/06.
+//
+
+import SwiftUI
+
+@main
+struct buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
