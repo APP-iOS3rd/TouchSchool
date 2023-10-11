e5111acb3d3ea4f5099075566d90afe617104459
Choe DongHo <hamfan524@naver.com>
2023-10-11 14:51:12 +0900

    feat: Add RankView

--- TouchSchool/TouchSchool/iOS/Rank/RankVM.swift
+++ TouchSchool/TouchSchool/iOS/Rank/RankVM.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+import SwiftUI
+
+class RankVM: ObservableObject {
+    
+}
+
--- TouchSchool/TouchSchool/iOS/Rank/RankView.swift
+++ TouchSchool/TouchSchool/iOS/Rank/RankView.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct RankView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
