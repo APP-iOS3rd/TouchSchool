d6e885c8646a9b9d1f5a03aed5287841004f4d4f
RapidSloth <yud1230@naver.com>
2023-10-16 13:32:16 +0900

    feat: MainView 구현

--- TouchSchool/TouchSchool/iOS/Main/MainView.swift
+++ TouchSchool/TouchSchool/iOS/Main/MainView.swift
@@ -9,9 +9,56 @@ import Foundation
 import SwiftUI
 
 struct MainView: View {
+    
     var body: some View {
-        VStack{
+        ZStack{
+            Color(red: 132/255, green: 194/255, blue: 65/255).edgesIgnoringSafeArea(.bottom)
+            
+            VStack{
+                Image("school1")
+                    .resizable()
+                    .frame(height: 200)
+                    .edgesIgnoringSafeArea(.top)
+                
+                Text("TouchSchool")
+                    .font(.system(size: 60, weight: .black, design: .rounded))
+                    .fontWeight(.heavy)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("게임 시작")
+                        .font(.largeTitle)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .padding()
+                        .background(Color.mint)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .overlay(RoundedRectangle(cornerRadius: 40)
+                            .stroke(Color.mint, lineWidth: 3)
+                                 )})
+                .padding()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("랭킹 보기")
+                        .font(.largeTitle)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .padding()
+                        .background(Color.yellow)
+                        .cornerRadius(40)
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .overlay(RoundedRectangle(cornerRadius: 40)
+                            .stroke(Color.yellow, lineWidth: 3)
+                                 )})
+     
+     
+    
+                Spacer()
+            }
+            
             
         }
     }
+}
+#Preview {
+    MainView()
 }
