5fff93ec6efa96c05cb81a1aa495b8cbe60ca3a5
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2023-11-07 17:47:55 +0900

    rank 화면 추가

--- TouchSchool/TouchSchool/iOS/Rank/RankVM.swift
+++ TouchSchool/TouchSchool/iOS/Rank/RankVM.swift
@@ -4,11 +4,86 @@
 //
 //  Created by 최동호 on 10/11/23.
 //
-
+//
 import Foundation
-import SwiftUI
+import FirebaseFirestore
+import FirebaseCore
 
-class RankVM: ObservableObject {
-    
+struct SchoolInfo: Identifiable {
+    var id: String
+    var name: String
+    var address: String
+    var count: Int
 }
 
+class RankVM: ObservableObject {
+    @Published var mySchoolInfos = [SchoolInfo]()
+    
+    private let db: Firestore
+    
+    init() {
+        db = Firestore.firestore()
+    }
+    
+    func fetchSchools(mySeq: String) {
+        db.collection("schools")
+            .whereField("seq", isEqualTo: mySeq)
+            .order(by: "count", descending: true)
+            .limit(to: 100)
+            .getDocuments { (querySnapshot, error) in
+                if let error = error {
+                    print("문서 가져오는 중 오류 발생: \(error)")
+                } else if let documents = querySnapshot?.documents {
+                    var schoolInfos = [SchoolInfo]()
+                    for document in documents {
+                        print("\(document.documentID) => \(document.data())")
+                        
+                        let mySchoolName = document.data()["name"] as? String ?? ""
+                        let mySchoolAdres = document.data()["adres"] as? String ?? ""
+                        let mySchoolCnt = document.data()["count"] as? Int ?? 0
+                        let mySchoolId = document.documentID
+                        
+                        let schoolInfo = SchoolInfo(id: mySchoolId, name: mySchoolName, address: mySchoolAdres, count: mySchoolCnt)
+                        schoolInfos.append(schoolInfo)
+                    }
+                    self.mySchoolInfos = schoolInfos
+                }
+            }
+    }
+}
+
+
+//import Foundation
+//import FirebaseFirestoreSwift
+//import FirebaseFirestore
+//import FirebaseCore
+//
+//class RankVM: ObservableObject {
+//
+//    @Published var mySchoolName: String = ""
+//    @Published var mySchoolAdres: String = ""
+//    @Published var mySchoolCnt: Int = 0
+//    @Published var mySchoolId: String = ""
+//
+//    func fetchSchools() {
+//        let db = Firestore.firestore()
+//
+//        db.collection("schools")
+//                    .order(by: "count", descending: true)
+//                    .limit(to: 100)
+//                    .getDocuments { (querySnapshot, error) in
+//                        if let error = error {
+//                            print("문서 가져오는 중 오류 발생: \(error)")
+//                        } else if let documents = querySnapshot?.documents {
+//                            for document in documents {
+//                                print("\(document.documentID) => \(document.data())")
+//
+//                    self.mySchoolName = document.data()["name"] as? String ?? ""
+//                    self.mySchoolAdres = document.data()["adres"] as? String ?? ""
+//                    self.mySchoolCnt = document.data()["count"] as? Int ?? 0
+//                    self.mySchoolId = document.documentID
+//                }
+//            }
+//        }
+//    }
+//}
--- TouchSchool/TouchSchool/iOS/Rank/RankView.swift
+++ TouchSchool/TouchSchool/iOS/Rank/RankView.swift
@@ -1,3 +1,4 @@
+
 //
 //  RankView.swift
 //  TouchSchool
@@ -8,10 +9,47 @@
 import Foundation
 import SwiftUI
 
+
 struct RankView: View {
+    
+    @ObservedObject private var schoolViewModel = RankVM()
+    @State private var schoolInfos: [SchoolInfo] = []
+    
+    
     var body: some View {
-        VStack{
-            
+        
+        ZStack{
+            Color(red: 132/255, green: 194/255, blue: 65/255).edgesIgnoringSafeArea(.bottom)
+            //꾸밈화면
+            VStack{
+                VStack{
+                ZStack{
+                    Image("school1")
+                        .resizable()
+                        .frame(height: 200)
+                        .edgesIgnoringSafeArea(.top)
+                    
+                    ZStack{
+                        //이부분은 좀더 잘 보이게 꾸밀 예정.
+                        Text("학교 순위")
+                            .fontWeight(.bold)
+                            .font(.system(size: 45))
+                            .foregroundColor(.black)
+//                            .background(.white)
+
+                    }
+                    .offset(y: 85)
+                }
+                
+                    // 학교 순위리스트
+                    List(schoolInfos) { schoolInfo in
+                        VStack(alignment: .leading) {
+                            Text(schoolInfo.name).font(.title)
+                        }
+                    }
+                    .scrollContentBackground(.hidden)
+                }
+            }
         }
     }
 }
